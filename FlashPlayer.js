bf3d66e89abde9f094f4ba418cedf2e91623ab17
Jeppe Burchardt <jebu@dr.dk>
2014-06-05 15:22:49 +0200

    use FlashObject for flash version detection

--- FlashPlayer.js
+++ FlashPlayer.js
@@ -1,5 +1,5 @@
 /* jshint devel: true */
-/* global define: true, ActiveXObject: true */
+/* global define: true */
 
 /**
  * TODO: dont use DR namespace for swf path
@@ -57,47 +57,7 @@ define('dr-media-flash-video-player', ['dr-media-class', 'dr-media-video-player'
         VideoPlayer.prototype.build.call(this);
     };
     FlashPlayer.isFlashOutdated =  function() {
-        return FlashPlayer.getFlashMajorVersion() < 10 || (FlashPlayer.getFlashMajorVersion() == 10 && FlashPlayer.getFlashMinorVersion() < 2);
-    };
-    FlashPlayer.getFlashMajorVersion = function() {
-        var fullVersion = FlashPlayer.getFlashFullVersion();
-        return fullVersion.split(',')[0];
-    };
-    FlashPlayer.getFlashMinorVersion = function() {
-        var fullVersion = FlashPlayer.getFlashFullVersion();
-
-        if (fullVersion.split(',').length > 1) {
-            return fullVersion.split(',')[1];
-        }
-
-        return '0';
-    };
-    FlashPlayer.getFlashFullVersion = function() {
-        // ie
-        try {
-            try {
-                // avoid fp6 minor version lookup issues
-                // see: http://blog.deconcept.com/2006/01/11/getvariable-setvariable-crash-internet-explorer-flash-6/
-                var axo = new ActiveXObject('ShockwaveFlash.ShockwaveFlash.6');
-                try {
-                    axo.AllowScriptAccess = 'always';
-                } catch(e) {
-                    return '6,0,0';
-                }
-            } catch(e) {
-            }
-
-            return new ActiveXObject('ShockwaveFlash.ShockwaveFlash').GetVariable('$version').replace(/\D+/g, ',').match(/^,?(.+),?$/)[1];
-            // other browsers
-        } catch(e) {
-            try {
-                if(navigator.mimeTypes['application/x-shockwave-flash'].enabledPlugin){
-                    return (navigator.plugins['Shockwave Flash 2.0'] || navigator.plugins['Shockwave Flash']).description.replace(/\D+/g, ',').match(/^,?(.+),?$/)[1];
-                }
-            } catch(error) { console.error('Unable to detect flash player version'); }
-        }
-
-        return '0,0,0';
+        return FlashObject.getFlashMajorVersion() < 10 || (FlashObject.getFlashMajorVersion() == 10 && FlashObject.getFlashMinorVersion() < 2);
     };
     FlashPlayer.prototype.showFlashErrorMessage = function () {
         this.displayError('obsolete_flash_player');
