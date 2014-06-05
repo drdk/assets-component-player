4bafb95b892aeafc34d4cd9fde27073fe6a008c8
Jeppe Burchardt <jebu@dr.dk>
2014-06-05 15:21:11 +0200

    add flash version to FlashObject

--- utils/FlashObject.js
+++ utils/FlashObject.js
@@ -1,5 +1,5 @@
 /* jshint devel:true */
-/* global define: true, escape: true, __flash__argumentsToXML: true */
+/* global define: true, escape: true, __flash__argumentsToXML: true, ActiveXObject: true */
 
 /** 
  * Replacement for mootool's Swiff class. 
@@ -66,6 +66,45 @@ define('dr-media-flash-object', ['swfobject'], function (Swfobject) {
         };
 
     }
+
+    FlashObject.getFlashMajorVersion = function() {
+        var fullVersion = FlashObject.getFlashFullVersion();
+        return fullVersion.split(',')[0];
+    };
+    FlashObject.getFlashMinorVersion = function() {
+        var fullVersion = FlashObject.getFlashFullVersion();
+        if (fullVersion.split(',').length > 1) {
+            return fullVersion.split(',')[1];
+        }
+        return '0';
+    };
+    FlashObject.getFlashFullVersion = function() {
+        // ie
+        try {
+            try {
+                // avoid fp6 minor version lookup issues
+                // see: http://blog.deconcept.com/2006/01/11/getvariable-setvariable-crash-internet-explorer-flash-6/
+                var axo = new ActiveXObject('ShockwaveFlash.ShockwaveFlash.6');
+                try {
+                    axo.AllowScriptAccess = 'always';
+                } catch(e) {
+                    return '6,0,0';
+                }
+            } catch(e) {
+            }
+
+            return new ActiveXObject('ShockwaveFlash.ShockwaveFlash').GetVariable('$version').replace(/\D+/g, ',').match(/^,?(.+),?$/)[1];
+            // other browsers
+        } catch(e) {
+            try {
+                if(navigator.mimeTypes['application/x-shockwave-flash'].enabledPlugin){
+                    return (navigator.plugins['Shockwave Flash 2.0'] || navigator.plugins['Shockwave Flash']).description.replace(/\D+/g, ',').match(/^,?(.+),?$/)[1];
+                }
+            } catch(error) { console.error('Unable to detect flash player version'); }
+        }
+
+        return '0,0,0';
+    };
 
     return FlashObject;
 });
