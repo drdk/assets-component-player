781a81349dc69d2ef9c23ad16c0a8804d31e043b
Philip Heyde <phey@dr.dk>
2014-06-12 15:42:22 +0200

    Fix and close #130

--- widget/dr-widget-video-playlist.js
+++ widget/dr-widget-video-playlist.js
@@ -68,7 +68,11 @@ define("dr-widget-video-playlist", ["dr-media-player-factory"], function (Player
             event.preventDefault();
             this.deselectAll();
             this.selectItem(selectedElement);
+            this.videoPlayer.programcardResult = null; // otherwise the old program card result will be used
             this.videoPlayer.updateOptions({
+                'appData': {
+                    'autoPlay': true
+                },
                 'videoData': {
                     'resource': selectedElement.get('data-resource'),
                     'image': selectedElement.get('data-image'),
