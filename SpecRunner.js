568898ac00651dd851515f4af8c8ef59c21df877
Jeppe Burchardt <jebu@dr.dk>
2014-09-24 16:17:09 +0200

    get jasmine to play nice with require

--- SpecRunner.js
+++ SpecRunner.js
@@ -4,12 +4,14 @@ require.config({
         'jasmine': 'lib/jasmine-2.0.3/jasmine',
         'jasmine-html': 'lib/jasmine-2.0.3/jasmine-html',
         'jasmine-ajax': 'lib/jasmine-2.0.3/mock-ajax',
+        'jasmine-boot': 'lib/jasmine-2.0.3/boot',
         'dr-media-player-factory': 'dist/assets-component-player',
         'dr-media-gemius-implementation': 'dist/assets-component-player',
         'dr-media-gemius-implementation-test': 'dist/assets-component-player',
         'dr-media-sola-implementation': 'dist/assets-component-player',
         'dr-media-springstreams-implementation': 'dist/assets-component-player',
         'dr-media-hash-implementation': 'dist/assets-component-player',
+        'dr-media-psdb-utilities': 'dist/assets-component-player',
         'dr-widget-video-player-factory': 'dist/assets-component-player',
         'dr-widget-video-player': 'dist/assets-component-player',
         'dr-widget-video-playlist': 'dist/assets-component-player',
@@ -20,6 +22,7 @@ require.config({
         'dr-media-audio-player': 'dist/assets-component-player',
         'dr-media-html5-audio-player': 'dist/assets-component-player',
         'dr-media-flash-audio-player': 'dist/assets-component-player',
+        'dr-widget-media-dom-helper': 'dist/assets-component-player',
         'dr-error-messages': 'dist/assets-component-player',
         'swfobject': 'lib/swfobject-1.5/swfobject',
         'swfobject2': 'lib/swfobject-2.2/swfobject',
@@ -38,21 +41,23 @@ require.config({
         'jasmine-ajax': {
             'deps': ['jasmine-html'],
             'exports': 'jasmine'
+        },
+        'jasmine-boot': {
+            'deps': ['jasmine-html'],
+            'exports': 'jasmine'
         }
     }
 });
 
-require(['jasmine-html'], function (jasmine) {
-
-    var jasmineEnv = jasmine.getEnv();
-    jasmineEnv.updateInterval = 1000;
+require(['jasmine-boot'], function (jasmine) {
 
     var specs = [
         'spec/GemiusimplementationSpec'
     ];
     
     require(specs, function (spec) {
-        jasmineEnv.execute();
+        window.executeTests();
     });
+
 
 });
